336e1f2645c0e383903611eae2af0053d0044830
Beomsoo Ko <go824001@naver.com>
2025-08-11 00:57:51 +0900

    controller 설정
    controller 설정

--- final22c/src/main/java/com/ex/final22c/controller/PerfumeController.java
+++ final22c/src/main/java/com/ex/final22c/controller/PerfumeController.java
@@ -1,6 +1,7 @@
 package com.ex.final22c.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ex.final22c.service.PerfumeService;
 
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/main/*")
 public class PerfumeController {
 
     private PerfumeService perfumeService;
