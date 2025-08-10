bfa729eb6690e633f4e3d664d70d1edc9f26327a
Beomsoo Ko <go824001@naver.com>
2025-08-10 14:47:50 +0900

    프로젝트 시작 준비
    프로젝트 시작 준비

---
+++ final22c/src/main/java/com/ex/final22c/Final22cApplication.java
@@ -0,0 +1,13 @@
+package com.ex.final22c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Final22cApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Final22cApplication.class, args);
+	}
+
+}
---
+++ final22c/src/test/java/com/ex/final22c/Final22cApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.final22c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Final22cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
