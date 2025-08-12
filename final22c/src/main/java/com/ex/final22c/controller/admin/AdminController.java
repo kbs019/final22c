9d5aec804149df14bfa11b22d8542ccaa59ed5ab
choi-jh1 <star6248@naver.com>
2025-08-12 18:22:51 +0900

    DDDD

---
+++ final22c/src/main/java/com/ex/final22c/controller/admin/AdminController.java
@@ -0,0 +1,41 @@
+package com.ex.final22c.controller.admin;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ex.final22c.data.user.Users;
+import com.ex.final22c.service.admin.AdminService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin/")
+public class AdminController {
+	private final AdminService adminService;
+	
+	// 회원목록
+	@GetMapping("userList")
+	public String userList(Model model,@RequestParam(value="page",defaultValue="0")int page,@RequestParam(value="kw" ,defaultValue="") String kw) {
+		Page<Users> user = adminService.getList(page,kw);
+		model.addAttribute("user",user);
+		model.addAttribute("kw",kw);
+		return "admin/userList";
+	}
+	
+	// 회원 정지
+	@PostMapping("changeStatus")
+	@ResponseBody
+	public ResponseEntity<String> changeUserStatus(@RequestParam("username") String username, @RequestParam("status") String status){
+		this.adminService.banned(status, username);
+		return ResponseEntity.ok("변경 성공");
+		
+	}
+}
--- final22c/src/main/java/com/ex/final22c/repository/user/UserRepository.java
+++ final22c/src/main/java/com/ex/final22c/repository/user/UserRepository.java
@@ -2,10 +2,15 @@ package com.ex.final22c.repository.user;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 
 import com.ex.final22c.data.user.Users;
-
+@Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
 
     Optional<Users> findByUserName(String userName);
@@ -13,6 +18,9 @@ public interface UserRepository extends JpaRepository<Users, Long> {
     Optional<Users> findByEmail(String email);
 
     Optional<Users> findByPhone(String phone);
+    
+    // 유저목록 (페이징,검색)
+    Page<Users> findAll(Specification<Users> spec,Pageable pageable);
 }
 
 
