968a312546bee06df3a6f6f29ed76830728bfe42
choi-jh1 <star6248@naver.com>
2025-08-18 18:21:05 +0900

    dddddd

---
+++ final22c/src/main/java/com/ex/final22c/repository/productRepository/MainNoteRepository.java
@@ -0,0 +1,9 @@
+package com.ex.final22c.repository.productRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ex.final22c.data.product.MainNote;
+
+public interface MainNoteRepository extends JpaRepository<MainNote,Integer>{
+
+}
