4001816076ed390d97fac4b88daf02dd86e06c30
Beomsoo Ko <go824001@naver.com>
2025-08-23 14:18:09 +0900

    purchase 설정 추가
    설정 추가

--- final22c/src/main/java/com/ex/final22c/repository/purchaseRepository/PurchaseRequestRepository.java
+++ final22c/src/main/java/com/ex/final22c/repository/purchaseRepository/PurchaseRequestRepository.java
@@ -1,10 +1,13 @@
 package com.ex.final22c.repository.purchaseRepository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.ex.final22c.data.product.Product;
 import com.ex.final22c.data.purchase.PurchaseRequest;
 @Repository
 public interface PurchaseRequestRepository extends JpaRepository<PurchaseRequest,Long>{
-
+    Optional<PurchaseRequest> findByProduct(Product product);
 }
